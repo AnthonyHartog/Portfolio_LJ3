6e54de5bdd702c8b862b038f34f5f3b55c165164
Anthony <93596000+AnthonyHartog@users.noreply.github.com>
2023-09-21 10:25:22 +0200

    Delete
    De front-end van een bericht te verwijderen en de javascript

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -11,28 +11,31 @@
                 @endforeach
                 <h1 id="projectname">{{$project->title}}</h1>   
                 <p>{{$project->description}}</p> 
-                <button id="showdestroy" style="display: show;">Verwijderen</button>
-                <form id="deleteform" style="display: none;" action="{{ route('project.destroy', $project->id) }}" method="Post">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" style="font-weight: bold;">Verwijder nu</button>
-                </form>
-                <script>
-                //Kijk of er opgedrukt is
-                document.getElementById('showdestroy').addEventListener('click', function(){
-                        //Haal de project naam op
-                        let projectName = document.getElementById('projectname').textContent;
+                <a href="{{ route('project.edit', $project->id) }}">Bewerken</a>
+                @if(Auth::user() != NULL)
+                    <button id="showdestroy" style="display: show;">Verwijderen</button>
+                    <form id="deleteform" style="display: none;" action="{{ route('project.destroy', $project->id) }}" method="Post">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" style="font-weight: bold;">Verwijder nu</button>
+                    </form>
+                    <script>
+                    //Kijk of er opgedrukt is
+                    document.getElementById('showdestroy').addEventListener('click', function(){
+                            //Haal de project naam op
+                            let projectName = document.getElementById('projectname').textContent;
 
-                        //Vraag of ze het zeker weten
-                        let answer = prompt('Weet je het zeker? Typ het projectnaam');
-                        //Kijken of het antwoord goed is
-                        if(answer == projectName){
-                            //Laat het form zien en de a verdwijnen
-                            document.getElementById('deleteform').style.display = "block";
-                            document.getElementById('showdestroy').style.display = "none";
-                        }
-                    })
-                </script>
+                            //Vraag of ze het zeker weten
+                            let answer = prompt('Weet je het zeker? Typ het projectnaam');
+                            //Kijken of het antwoord goed is
+                            if(answer == projectName){
+                                //Laat het form zien en de a verdwijnen
+                                document.getElementById('deleteform').style.display = "block";
+                                document.getElementById('showdestroy').style.display = "none";
+                            }
+                        })
+                    </script>
+                @endif
         </div>
     </div>
     
